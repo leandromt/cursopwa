c9726e8ce53554d2523fe93d5788c0bec0fa3395
Leandro Mendes Tavares <tec.LeandroMendes@opovonet.com.br>
2018-04-13 17:35:15 -0300

    WS Edit

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,35 @@
-let cacheName = 'notes-son-v1.0.0';
+let cacheName = 'notes-son-v.1.0.5';
 let filesToCache = [
-	'',
-	'index.html',
-	'css/colors.css',
-	'css/styles.css',
-	'js/object.observe.polyfill.js',
-	'js/array.observe.polyfill.js',
-	'js/scripts.js',
-	'service-worker.js'
+    './',
+    'index.html',
+    'css/colors.css',
+    'css/styles.css',
+    'js/array.observe.polyfill.js',
+    'js/object.observe.polyfill.js',
+    'js/scripts.js'
 ];
 
-
-console.log('1');
-
-self.addEventListener('install', function(e){
-
-	console.log('[ServiceWorker] Installer');
-
-	e.waitUntil(
-		caches.open(cacheName).then(function(cache){
-			console.log('[ServiceWorker] Caching app shell');
-			return cache.addAll(filesToCache);
-		})
-	);
-
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Installer');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 
-console.log('2');
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+        	return Promise.all(keyList.map(function(key){
+        		if(key != cacheName){
+        			console.log('[ServiceWorker] Removing old cache');
+        			return caches.delete(key);
+        		}
+        	}))
+        })
+    );
+});
