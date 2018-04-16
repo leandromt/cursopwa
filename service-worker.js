719d0bc9e9d1abf24d04a944c2930cb2bb8e2a01
Leandro Mendes Tavares <tec.LeandroMendes@opovonet.com.br>
2018-04-16 17:36:08 -0300

    Add icons

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,19 @@
-let cacheName = 'notes-son-v.1.0.5';
+// VERSAO DO CACHE
+let cacheName = 'notes-son-v.1.0.8';
+
+
+// ARQUIVOS DE CACHE
 let filesToCache = [
     './',
     'index.html',
-    'css/colors.css',
     'css/styles.css',
     'js/array.observe.polyfill.js',
     'js/object.observe.polyfill.js',
     'js/scripts.js'
 ];
 
+
+// CRIA O CACHE NA INSTALACAO DO SERVICE WORKER
 self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Installer');
     e.waitUntil(
@@ -20,16 +25,32 @@ self.addEventListener('install', function (e) {
 });
 
 
+
+
+// ATUALIZA O CACHE DO CASO TENHA ALTARCAO NO CACHENAME
 self.addEventListener('activate', function (e) {
     console.log('[ServiceWorker] Activate');
     e.waitUntil(
-        caches.keys().then(function(keyList) {
-        	return Promise.all(keyList.map(function(key){
-        		if(key != cacheName){
-        			console.log('[ServiceWorker] Removing old cache');
-        			return caches.delete(key);
-        		}
-        	}))
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+
+
+
+// REALIZA O OFFLINE DO SERVICE WORKER
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
         })
     );
 });
